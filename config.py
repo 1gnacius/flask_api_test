faf75038ddeb3cc3fe2fda1bbf9598bfc1f08641
Ignacio Valdebenito <ignacius@Ignacios-MacBook-Pro.local>
2024-06-30 21:40:29 -0400

    push to render

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-# config.py
+import os
 
 DATABASE = {
-    'host': 'dpg-cpvjt7uehbks73dv6ljg-a.oregon-postgres.render.com',
-    'database': 'machine_pulses_stats',
-    'user': 'machine_pulses_stats_user',
-    'password': 'a944wDdSAsT0OauBPywxx579r8sQkFRm'
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD')
 }
