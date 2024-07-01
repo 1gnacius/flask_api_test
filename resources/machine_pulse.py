bfeb6acd246112067d21dbbca3419c1301170d9b
Ignacio Valdebenito <ignacius@Ignacios-MacBook-Pro.local>
2024-06-30 22:29:02 -0400

    machine pulse model

--- resources/machine_pulse.py
+++ resources/machine_pulse.py
@@ -1,5 +1,7 @@
 # resources/item.py
 
+from datetime import datetime
+
 from flask_restful import Resource, reqparse
 
 import db
@@ -14,10 +16,19 @@ class MachinePulse(Resource):
         machine_pulses = db.get_all_machine_pulses()
         return {'machine_pulses': machine_pulses}
 
-    def post(self):
+    def post(self): 
         args = parser.parse_args()
         ip = args['ip']
-        executed_at = args['executed_at']
+        executed_at_timestamp = args['executed_at']
         machine_id = args['machine_id']
-        machine_pulse_id = db.execute_machine_pulse(ip, executed_at, machine_id)
+        
+        # Convertir el timestamp a objeto datetime
+        executed_at = datetime.fromtimestamp(executed_at_timestamp)
+        
+        # Convertir datetime a formato ISO 8601
+        executed_at_iso = executed_at.isoformat()
+        
+        # Guardar la pulsación de la máquina en la base de datos
+        machine_pulse_id = db.execute_machine_pulse(ip, executed_at_iso, machine_id)
+        
         return {'id': machine_pulse_id}, 201
