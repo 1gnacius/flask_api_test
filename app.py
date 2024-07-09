d791976cac8c9165761ecc6d5e0defea6cf2d590
Ignacio Valdebenito <ignacius@Ignacios-MacBook-Pro.local>
2024-07-09 01:01:11 -0400

    log

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 from resources.item import ItemList
 from resources.machine_pulse import MachinePulse
 from resources.session import SessionExecution
-from resources.task import Tasks
+from resources.task import TaskList
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,7 +14,7 @@ api = Api(app)
 api.add_resource(ItemList, '/items')
 api.add_resource(MachinePulse, '/machine_pulse')
 api.add_resource(SessionExecution, '/session_execution')
-api.add_resource(Tasks, '/tasks')
+api.add_resource(TaskList, '/tasks')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- resources/task.py
+++ resources/task.py
@@ -17,7 +17,7 @@ def json_type(value):
     except ValueError:
         raise ValueError('Invalid JSON')
 
-class Tasks(Resource):
+class TaskList(Resource):
     def get(self):
         tasks = db.get_all_tasks()
         return {'tasks': tasks}
